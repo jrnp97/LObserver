67a5f4769cb23574dc4a61ad52c8fe842814f4f6
jrnp97 <jrnp1997@gmail.com>
2020-10-19 03:34:37 -0300

    [IMPROVEMENT] Implementing threadPool and rout of process to improve process speed

--- src/main.py
+++ src/main.py
@@ -1,26 +1,31 @@
 """ Main script """
 import re
+import os
 import time
 import datetime
-import threading
+import abc
 import logging.config
 import multiprocessing
 import urllib.request as request
 from urllib.error import URLError
 
 import queue
+
+from concurrent.futures import ThreadPoolExecutor
+
 from multiprocessing import JoinableQueue as PQueue  # Process Queue
-from queue import Queue  # Thread Queue
 
 from collections.abc import Iterable
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
+import pymongo
 from pymongo import MongoClient
 
 from bs4 import BeautifulSoup
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 log_config = {
     'version': 1,
     'formatters': {
@@ -53,8 +58,13 @@ logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('main')
 
-request_queue = Queue()
 process_queue = PQueue()
+
+client = MongoClient(
+    ''
+)
+db = client.linio_data
+db_products = db.products
 
 home_url = 'https://www.linio.com.co/'
 
@@ -62,7 +72,10 @@ home_url = 'https://www.linio.com.co/'
 def download_b64_image(url):
     options = Options()
     options.headless = True
-    driver = webdriver.Firefox(firefox_options=options, executable_path='data/geckodriver')
+    driver = webdriver.Firefox(
+        firefox_options=options,
+        executable_path=os.path.join(BASE_DIR, 'data', 'geckodriver'),
+    )
     driver.get(url)
     return driver.get_screenshot_as_base64()
 
@@ -73,6 +86,7 @@ def get_product_url(sku):
     return res.url
 
 
+"""LEGACY
 def get_database(db_name='ldata'):
     from codernitydb3.database import Database
     db = Database(db_name)
@@ -81,28 +95,39 @@ def get_database(db_name='ldata'):
     else:
         db.open()
     return db
+"""
 
 
-def get_main_content(silent=True):
-    while True:
-        data = request_queue.get()  # BLOCKING
-        try:
-            url = data['url']
-            logger.debug(f'Requesting: {url}')
-            res = request.urlopen(url=url)
-            content = res.read()
-            process_queue.put({
-                'url': data['url'],
-                'html_content': content,
-                'process': data['callback'],
-            })
-        except URLError:
-            if silent:
-                return None
-            logger.exception('Error getting server data')
-            raise
-        finally:
-            request_queue.task_done()
+def insert_product(data, silent=True):
+    product = dict(data['product'])
+    logger.debug(f'INSERTING: {product}')
+    if not db_products.find_one({'sku': product['sku']}):
+        db_products.insert_one(product)
+    else:
+        db_products.find_and_modify({
+            'query': {'sku': product['sku']},
+            'update': [{
+                '$set': {'prices': {'$concatArrays': ["$prices", product['prices']]}}
+            }]
+        })
+
+
+def get_main_content(data, silent=True):
+    try:
+        url = data['url']
+        logger.debug(f'Requesting: {url}')
+        res = request.urlopen(url=url)
+        content = res.read()
+        process_queue.put({
+            'url': data['url'],
+            'html_content': content,
+            'process': data['callback'],
+        })
+    except URLError:
+        if silent:
+            return None
+        logger.exception('Error getting server data')
+        raise
 
 
 def parse_category(base_url, categories, silent=True):
@@ -140,7 +165,14 @@ def parse_product(product_div, silent=True):
             meta.attrs['itemprop']: meta.attrs['content'] for meta in product_div.findAll('meta')
         }
         data['full_url'] = f'{home_url}{data["url"]}'
-        data['registered_date'] = datetime.datetime.utcnow()
+        prices = [
+            {
+                'price': data.pop('price', 0),
+                'priceCurrency': data.pop('priceCurrency', 'UNDEFINED'),
+                'registered_date': datetime.datetime.utcnow(),
+            },
+        ]
+        data['prices'] = prices
         return data
     except (AttributeError, KeyError):
         if silent:
@@ -197,12 +229,18 @@ def extract_pages(base_url, content, silent=True):
     return [f'{base_url}?page={page}' for page in range(2, max_page + 1)]
 
 
-class CategoryProcess:
+class Strategy(abc.ABC):
+    """ Strategy Interface """
 
-    def process(self, **kwargs):
+    @abc.abstractmethod
+    def process(self, pipe, html_content, data):
+        """ Method to define logic to implement """
+
+
+class CategoryProcess(Strategy):
+
+    def process(self, pipe, html_content, data):
         logger.debug('Extracting Categories')
-        pipe = kwargs['pipe']
-        html_content = kwargs['html_content']
         categories = parse_category(
             base_url=home_url,
             categories=extract_categories(
@@ -213,18 +251,16 @@ class CategoryProcess:
             logger.debug('Sending jobs.')
             pipe.send({
                 'url': cat_url,
+                'route': 'request',
                 'callback': 'pages'
             })
         return True
 
 
-class PageProcess:
+class PageProcess(Strategy):
 
-    def process(self, **kwargs):
+    def process(self, pipe, html_content, data):
         logger.debug('Processing Pages...')
-        pipe = kwargs['pipe']
-        html_content = kwargs['html_content']
-        data = kwargs['data']
         cat_url = data['url']
         pages = [cat_url]
         pages.extend(
@@ -236,21 +272,23 @@ class PageProcess:
         for cat_page in pages:
             pipe.send({
                 'url': cat_page,
+                'route': 'request',
                 'callback': 'products',
             })
         return True
 
 
-class ProductProcess:
+class ProductProcess(Strategy):
 
-    def process(self, **kwargs):
+    def process(self, pipe, html_content, data):
         logger.debug('Processing Products...')
-        html_content = kwargs['html_content']
-        db = kwargs['database']
         for product in extract_category_products(category_html=html_content):
             if not product:
                 continue
-            db.products.insert_one(parse_product(product))
+            pipe.send({
+                'route': 'database',
+                'product': parse_product(product),
+            })
         return True
 
 
@@ -266,13 +304,11 @@ class Context:
             raise ValueError(f'Strategy: {strategy} not supported yet')
         self.strategy = self.strategies[strategy]()
 
-    def process(self, **kwargs):
-        return self.strategy.process(**kwargs)
+    def process(self, pipe, html_content, data):
+        return self.strategy.process(pipe, html_content, data)
 
 
 def process_worker(pipe):
-    client = MongoClient(port=27017)
-    db = client.linio_data  #get_database('parallel_process')
     while True:
         try:
             data = process_queue.get(timeout=30)  # Maximum 1/2 minutes
@@ -287,28 +323,40 @@ def process_worker(pipe):
                 pipe=pipe,
                 html_content=html_content,
                 data=data,
-                database=db,
             )
         finally:
             process_queue.task_done()
 
 
 if __name__ == '__main__':
-    logger.info('Start program')
+    logger.info(f'Start program {datetime.datetime.now()}')
+    db_products.create_index('sku', **{"unique": True})
     start = time.perf_counter()
     parent_conn, child_conn = multiprocessing.Pipe()
-    threading.Thread(target=get_main_content, daemon=True).start()
-    threading.Thread(target=get_main_content, daemon=True).start()
+    thread_pooling = ThreadPoolExecutor(max_workers=5)
     multiprocessing.Process(target=process_worker, args=(child_conn,), daemon=True).start()
-    request_queue.put({
+    router = {
+        'request': get_main_content,
+        'database': insert_product,
+    }
+    kwg = {
         'url': home_url,
         'callback': 'category',
-    })
+    }
+    thread_pooling.submit(router.get('request'), **{'data': kwg})
     while True:
         request_ = parent_conn.recv()
         if request_ == 'finish':
             break
-        request_queue.put(request_)
+        elif not isinstance(request_, dict):
+            continue
+        route = request_.pop('route', None)
+        fnc = router.get(route, None)
+        if not fnc:
+            continue
+        logger.debug(f'Sending to pool job: {fnc.__name__}')
+        thread_pooling.submit(fnc, **{'data': request_})
     parent_conn.close()
+    # TODO ThreadPooling finish before time.
     end = time.perf_counter()
     logger.info(f'Process Finished in {end - start}s :D')
